af2c3e5652485763b11d60c9c535bdffd1f96b06
Jessthesaver <70416069+Jessthesaver@users.noreply.github.com>
2022-11-14 12:23:29 -0600

    Add files via upload

--- 0-8-2.js
+++ 0-8-2.js
@@ -1,5 +1,3 @@
-let fns = [];
-
 let one = function(d) { // first function
     console.log('potato');
 }
@@ -10,26 +8,14 @@ let third = function(d){
     console.log('nappa')
 }
 
-fns.push(one);
-fns.push(two);
-fns.push(third);
+function centralTimer() {
+    let count = 0;
+    setInterval(()=>{
+        count++
+        if(count % 2 === 0) one();
+        if(count % 4 === 0) two();
+        if(count % 5 === 0) third();
+    }, 100*15)
+}
 
-let intervals = [5000, 10000, 15000];
-
-(function(fns, intervals) {
-let count = 0; 
-(function run () {
-     // time that function takes to execute 
-    if (count === intervals.length-1) {
-        fns[count]()
-        count = 0;
-        setTimeout(run, intervals[count] ); // function executes here 
-    } else {
-        setTimeout(run, intervals[count] );
-        fns[count]()
-        count++; 
-     // function executes here 
-    }
-})();
-//setTimeout(run,intervals[0]) //first call to make use of the first interval
-})(fns, intervals)
+centralTimer();
